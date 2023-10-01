a557cfbb25a4a431e0fd46c80b7ff71d78a8da33
Edgarmad <54693845+Edgarmad@users.noreply.github.com>
2023-10-01 12:39:33 -0600

    Update TaskItem.js

--- react/src/components/TaskItem.js
+++ react/src/components/TaskItem.js
@@ -1,31 +1,36 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import LikeButton from './LikeButton'; // Importa el nuevo componente
+import LikeButton from './LikeButton';
 
-function TaskItem({ task, onDelete }) {
+function TaskItem({ task, onLikeClick, onDelete }) {
   const [isLiked, setIsLiked] = useState(false);
-  const [likesCount, setLikesCount] = useState(task.likes_count);
+  const [showModal, setShowModal] = useState(false);
+  const [modalMessage, setModalMessage] = useState('');
 
   const handleLikeClick = () => {
     axios.post(`http://localhost:8000/api/tasks/${task.id}/like`)
       .then((response) => {
         setIsLiked(true);
-        setLikesCount(response.data.likesCount);
+        // Actualiza el contador de "likes" con el valor devuelto por el servidor
+        task.likes_count = response.data.likesCount;
+        onLikeClick(task); // Llama a la función onLikeClick con la tarea actualizada
       })
       .catch((error) => {
         console.error('Error al dar "Like":', error);
       });
   };
+
   const handleDeleteClick = () => {
     axios.delete(`http://localhost:8000/api/tasks/${task.id}`)
       .then((response) => {
-        // Llama a la función onDelete para eliminar la tarea del estado
+        // Llama a la función onDelete para eliminar la tarea del estado del componente padre
         onDelete(task.id);
       })
       .catch((error) => {
         console.error('Error al eliminar la tarea:', error);
       });
   };
+
   return (
     <li className="bg-white rounded shadow p-4">
       <h3 className="text-lg font-semibold">{task.title}</h3>
@@ -33,13 +38,13 @@ function TaskItem({ task, onDelete }) {
       <p className="text-gray-500">Fecha de Creación: {task.date}</p>
       <p className="text-gray-500">Estado: {task.state}</p>
       <p className="text-gray-500">Creador: {task.creator_name}</p>
-      <p className="text-gray-500">Likes: {likesCount}</p>
-      {/* Utiliza el componente LikeButton */}
+      <p className="text-gray-500">Likes: {task.likes_count}</p>
       <LikeButton
         isLiked={isLiked}
-        onLikeClick={handleLikeClick}
+        taskId={task.id}
+        onLikeClick={handleLikeClick} // Pasa la función handleLikeClick
       />
-       <button
+      <button
         className="bg-red-500 text-white rounded px-2 py-1 mt-2"
         onClick={handleDeleteClick}
       >
